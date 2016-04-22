bac2236348d3b5442428ee88b1ab8c0b89550032
misslio <lican929@163.com>
2016-04-22 05:28:32 -0700

    chand python

--- py_crawler/crawler.py
+++ py_crawler/crawler.py
@@ -1,16 +1,17 @@
-
+import socket
 
 def fetch(url):
 	sock = socket.socket()
-	sock.connect(('xkcd.com', 80))
-	request = 'GET {} HTTP/1.0\r\nHost: xkcd.com\r\n\r\n'.format(url)
+	sock.connect(('baidu.com', 80))
+	request = 'GET {} HTTP/1.0\r\nHost: baidu.com\r\n\r\n'.format(url)
 	sock.send(request.encode('ascii'))
 	response = b''
 	chunk = sock.recv(4096)
 	while chunk:
 		response += chunk
 		chunk = sock.recv(4096)
-
+	response.encode('utf-8')
+	print("%s\n"%(response))
 	# Page is now downloaded.
 	links = parse_links(response)
 	q.add(links)
