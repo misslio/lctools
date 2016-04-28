4dd02ca40cd1e86529cbcc6b1ee9b2e663e99a6c
misslio <lican929@163.com>
2016-04-28 01:21:31 -0700

    a version to test generator

--- py_crawler/crawler3.py
+++ py_crawler/crawler3.py
@@ -2,6 +2,34 @@ import asyncio
 import socket
 from selectors import DefaultSelector, EVENT_WRITE
 
+class Future:
+	def __init__(self)
+		self.result = None
+		self._callbacks = []
+
+	def add_done_callback(self, fn):
+		self._callbacks.append(fn)
+
+	def set_result(self, result)
+		self.result = result
+		for fn in self._callbacks
+			fn(self)
+
+class Task:
+	def __init__(self, coro):
+		self.coro = coro
+		f = Future()
+		f.set_result(None)
+		self.step(f)
+
+	def step(self, future)
+		try：
+			next_future = self.coro.send(future.result)
+		except StopIteration
+			return
+
+		next_future.add_done_callback(self.step)
+
 class Fetcher:
 
 	def __init_(self, url):
@@ -17,8 +45,46 @@ class Fetcher:
 		except BlockingIOError:
 			pass
 
-		selector.register(self.sock.fileno(), EVENT_WRITE,self.connected)
+		f = Future()
+		
+		def on_connected():
+			f.set_result(None)
 
+		selector.register(self.sock.fileno(), EVENT_WRITE,on_connected)
+
+		yield f
+
+		selector.unregister(sock.fileno())
+		print('connected!')
+
+		sock.send(request.encode('ascii'))
+		self.response = yield from read_all(sock)
+
+		while True:
+			f = Future()
+
+			def on_readable():
+				f.set_result(sock.recv(4096))
+
+			selector.register(sock.fileno(), EVENT_READ, on_readable)
+	
+			chunk = yield f
+			selector.unregister(sock.fileno())
+			if chunk:
+				self.response += chunk
+			else:
+				break
+
+	def read_all(sock):
+		response = []
+		chunk = yield from read(sock)
+		while chunk:
+			response.append(chunk)
+			chunk = yield from read(sock)
+
+		return b''.join(response)
+
+		
 	def connected(self, key, mask):
 		print('connected')
 		selector.unregister(key.fd)
